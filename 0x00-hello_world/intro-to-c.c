7517e1bb39126a59fde1f54a6fec0260f0da7ccb
Fking <farukadeko57@gmail.com>
2025-08-27 00:03:11 +0100

    Task 1: Added short note on why we need programming languages and C

---
+++ 0x00-hello_world/intro-to-c.c
@@ -0,0 +1,31 @@
+/*
+ * 1. Why do we need programming languages instead of just 0s and 1s?
+ *    Computers understand only understand the binary language (0s and 1s). If we wrote programs in pure binary,
+ *       it would be very difficult and time-consuming for people to read
+ *          and debug.So we use  Programming languages  as a bridge between humans and machines.
+ *             It allow developers to write instructions in a way that is easier to understand,
+ *                which the compiler translates them into 0s and 1s for the computer to understand.
+ *
+ *                2. Why do engineers need to learn C specifically?
+ *                   C is one of the most powerful and widely used programming languages.
+ *                      It allows direct control of hardware and memory, making it very efficient.
+ *                         Most modern languages (like Python, Java, C++) are built using C or based on its concepts.
+ *                            Engineers learn C because it teaches them how computers really work,
+ *                               and it is heavily used in system programming, embedded systems,
+ *                                  operating systems, and performance-critical applications.
+ *                                  *//*
+					 1. Why do we need programming languages instead of just 0s and 1s?
+					    Computers understand only understand the binary language (0s and 1s). If we wrote programs in pure binary,
+					       it would be very difficult and time-consuming for people to read
+					          and debug.So we use  Programming languages  as a bridge between humans and machines.
+						     It allow developers to write instructions in a way that is easier to understand,
+						        which the compiler translates them into 0s and 1s for the computer to understand.
+
+							2. Why do engineers need to learn C specifically?
+							   C is one of the most powerful and widely used programming languages.
+							      It allows direct control of hardware and memory, making it very efficient.
+							         Most modern languages (like Python, Java, C++) are built using C or based on its concepts.
+								    Engineers learn C because it teaches them how computers really work,
+								       and it is heavily used in system programming, embedded systems,
+								          operating systems, and performance-critical applications.
+									  */
